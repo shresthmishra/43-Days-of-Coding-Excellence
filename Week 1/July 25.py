fb89c37bb051f1baf3dff68a53e42d1c0ee18244
Shresth Mishra <shresthmishr@gmail.com>
2025-07-29 02:16:47 +0530

    Code July 25 problem

--- Week 1/July 25.py
+++ Week 1/July 25.py
@@ -4,3 +4,10 @@ Description: Given an array nums of integers, return how many of them contain an
 
 '''
 
+class Solution:
+    def findNumbers(self, nums: list[int]) -> int:
+        count = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                count += 1
+        return count
