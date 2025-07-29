c592a4dd519d4314fa60abe28ba9c94ee9ae6ed9
Shresth Mishra <shresthmishr@gmail.com>
2025-07-29 23:44:08 +0530

    Code July 28 problem

---
+++ Week 1/July 28.py
@@ -0,0 +1,20 @@
+'''
+# Problem: Two Sum
+# Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+solution = Solution()
+print(solution.twoSum([22,13,421,33,23], 55))
