b81e28369d1a4df138119b8d63157c624b48a7f5
Shresth Mishra <shresthmishr@gmail.com>
2025-07-29 22:55:43 +0530

    Update format in the files

--- Week 1/July 25.py
+++ Week 1/July 25.py
@@ -1,6 +1,6 @@
 '''
-Problem: Find Numbers with Even Number of Digits
-Description: Given an array nums of integers, return how many of them contain an even number of digits.
+# Problem: Find Numbers with Even Number of Digits
+# Description: Given an array nums of integers, return how many of them contain an even number of digits.
 
 '''
 
