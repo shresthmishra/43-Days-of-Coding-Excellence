831a001973039bdac31e51ebf61ac8b6f7ee3fe2
Shresth Mishra <shresthmishr@gmail.com>
2025-07-31 02:25:58 +0530

    Update code formats

--- Week 1/July 26.py
+++ Week 1/July 26.py
@@ -1,6 +1,6 @@
 '''
 # Problem: Fizz Buzz
-# Description: Given an integer n, return a string array answer (1-indexed) where:
+Description: Given an integer n, return a string array answer (1-indexed) where:
 
     answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
     answer[i] == "Fizz" if i is divisible by 3.
