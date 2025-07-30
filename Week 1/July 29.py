4be0895bf4ecf63626aba3144c1f58cbdfc1cf38
Shresth Mishra <shresthmishr@gmail.com>
2025-07-31 02:25:32 +0530

    Update code formats

---
+++ Week 1/July 29.py
@@ -0,0 +1,28 @@
+'''
+# Problem:
+Description: 
+'''
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1, 
+            'V': 5, 
+            'X': 10, 
+            'L': 50, 
+            'C': 100, 
+            'D': 500, 
+            'M': 1000
+        }
+        
+        n = len(s)
+        num = 0
+        
+        for i in range(n - 1):
+            if roman_map[s[i]] < roman_map[s[i+1]]:
+                num -= roman_map[s[i]]
+            else:
+                num += roman_map[s[i]]
+                
+        num += roman_map[s[n-1]]
+        return num
