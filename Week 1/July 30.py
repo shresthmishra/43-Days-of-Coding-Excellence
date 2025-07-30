3dc35d251b7e5d874180dbbabfb18091caaf37e9
Shresth Mishra <shresthmishr@gmail.com>
2025-07-31 02:32:26 +0530

    Code July 30 problem

---
+++ Week 1/July 30.py
@@ -0,0 +1,28 @@
+'''
+# Problem: Valid Parentheses
+Description: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+3. Every close bracket has a corresponding open bracket of the same type.
+
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping:
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+    
+solution = Solution()
+print(solution.isValid("((())"))
