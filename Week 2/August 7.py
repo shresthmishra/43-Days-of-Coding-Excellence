0593147b6f2aa83d0ea45b9522cdb473908cc918
Shresth Mishra <shresthmishr@gmail.com>
2025-08-08 02:07:21 +0530

    Code August 7 problem

---
+++ Week 2/August 7.py
@@ -0,0 +1,13 @@
+'''
+# Problem: Word Capitalization
+Description:
+
+Capitalization is writing a word with its first letter as a capital letter. 
+Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+'''
+
+word = input()
+result = word[0].capitalize() + (word[1::])
+print(result)
