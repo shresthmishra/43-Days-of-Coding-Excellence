88468d16db88dd80dd32412e41680859543ae9c1
Shresth Mishra <shresthmishr@gmail.com>
2025-08-14 09:31:17 +0530

    Code August 12 problem

---
+++ Week 3/August 12.py
@@ -0,0 +1,21 @@
+'''
+# Problem: Domino piling
+# Description: You are given a rectangular board of M × N squares. 
+Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. 
+You are allowed to rotate the pieces. 
+You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
+1. Each domino completely covers two squares.
+
+2. No two dominoes overlap.
+
+3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
+
+Find the maximum number of dominoes, which can be placed under these restrictions.
+'''
+
+m, n = map(int, input().split())
+
+total_squares = m * n
+max_dominoes = total_squares // 2
+
+print(max_dominoes)
