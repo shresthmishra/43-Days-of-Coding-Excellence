2e60d3e3d3db485f29d9dba9d63f83fa9fa1fe97
Shresth Mishra <shresthmishr@gmail.com>
2025-08-18 17:14:50 +0530

    Code August 17 problem

---
+++ Week 4/August 17.py
@@ -0,0 +1,15 @@
+'''
+# Problem: Soldier and Bananas
+Description:
+A soldier wants to buy w bananas in the shop. 
+He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
+
+He has n dollars. How many dollars does he have to borrow from his friend soldier to buy w bananas?
+'''
+
+x = float(input())
+n = int(input())
+
+result = x ** n
+
+print(f"{result:.5f}")
