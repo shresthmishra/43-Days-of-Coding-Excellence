ad0be461854ef09c2bc793191352b473ddc62a3f
Shresth Mishra <shresthmishr@gmail.com>
2025-08-26 23:11:30 +0530

    Code August 26 problem

---
+++ Week 5/August 26.py
@@ -0,0 +1,33 @@
+'''
+# Problem: Ultra-Fast Mathematician
+Description:
+Shapur was an extremely gifted student. He was great at everything including Combinatorics, Algebra, Number Theory, Geometry, Calculus, etc. 
+He was not only smart but extraordinarily fast! He could manage to sum 1018 numbers in a single second.
+
+One day in 230 AD Shapur was trying to find out if any one can possibly do calculations faster than him. 
+As a result he made a very great contest and asked every one to come and take part.
+
+In his contest he gave the contestants many different pairs of numbers. Each number is made from digits 0 or 1. 
+The contestants should write a new number corresponding to the given pair of numbers. 
+The rule is simple: The i-th digit of the answer is 1 if and only if the i-th digit of the two given numbers differ. 
+In the other case the i-th digit of the answer is 0.
+
+Shapur made many numbers and first tried his own speed. 
+He saw that he can perform these operations on numbers of length ∞ (length of a number is number of digits in it) in a glance! 
+He always gives correct answers so he expects the contestants to give correct answers, too. 
+He is a good fellow so he won't give anyone very big numbers and he always gives one person numbers of same length.
+
+Now you are going to take part in Shapur's contest. See if you are faster and more accurate.
+'''
+
+num1 = input()
+num2 = input()
+
+result = ""
+for i in range(len(num1)):
+    if num1[i] == num2[i]:
+        result += '0'
+    else:
+        result += '1'
+
+print(result)
